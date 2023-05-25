1a5f8dddc4e05e9d242c87a3fc91a168b35253bc
LukasBrezina <lukas.brezina@stud.fh-campuswien.ac.at>
2023-05-25 11:55:32 +0200

    server startet

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const sneaks = new SneaksAPI();
 
 app.get('/', function(req,res) {
     sneaks.getProducts("Yeezy Cinder", 1, function(err, products) {
-        res.json(products)
+        res.send(products)
     })
 })
 
