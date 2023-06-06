aa8058ad91921e24cd25bf20782bdb36214ebcc6
LeDanse <113842990+bitparcour@users.noreply.github.com>
2023-06-06 15:39:34 +0200

    index, server -3 lines

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,79 @@
 const express = require('express');
-const app = express();
-const path = require('path');
-const bodyParser = require("body-parser");
-//SzauleLogin/server/backendLoginAdmin.js
-const backendLoginAdmin = require('./backendLoginAdmin');
+const path = require('path')
+const app = express()
+const SneaksAPI = require('sneaks-api')
+const sneaks = new SneaksAPI()
+const shoeModel  = require('./shoe-model.js')
 
-// Serve static files from the "files" folder
-app.use(express.static(path.join(__dirname, 'files')));
+app.use(express.static(path.join(__dirname, 'Frontend')));
 
-// Handle GET request for the root URL
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'files', 'index.html'));
-});
+function filterShoes(shoes) {
+    var filtered = shoes.map(function(item) {      
+        return {
+            shoeName: item.shoeName,
+            brand: item.brand,
+            // colorWay: item.colorWay,
+            price: item.retailPrice,
+            thumbnail: item.thumbnail,
+            releaseDate: item.releaseDate
+        }
+    })
+    return filtered;
+}
 
-// Body parser middleware
-app.use(bodyParser.json());
+app.listen(3000)
+app.get('/all', function(req,res) {
+    let allSneaker = []
+    const getProduct1 = new Promise(function(resolve,reject) {
+        sneaks.getProducts("jordan", 5, function(err, products) {
+            products = filterShoes(products);
+            allSneaker.push(products);
+            resolve();
+        });
+     });
 
-// 
-//SzauleLogin/server/backendLoginAdmin.jse
-app.post('/api/login', backendLoginAdmin.handleLogin);
-app.post('/api/register', backendLoginAdmin.handleRegistration);
+    const getProduct2 = new Promise(function(resolve,reject) {
+        sneaks.getProducts("yeezy", 5, function(err, products) {
+            products = filterShoes(products);
+            allSneaker.push(products)
+            resolve();
+        });
+      });
+      const getProduct3 = new Promise(function(resolve,reject) {
+        sneaks.getProducts("nike", 5, function(err, products) {
+            products = filterShoes(products);
+            allSneaker.push(products)
+            resolve();
+        });
+      });
+      const getProduct4 = new Promise(function(resolve,reject) {
+        sneaks.getProducts("balance", 5, function(err, products) {
+            products = filterShoes(products);
+            allSneaker.push(products);
+            resolve();
+        })
+      })
+    
+    Promise.all([getProduct1, getProduct2, getProduct3, getProduct4])
+        .then(function() {
+          res.send(allSneaker);
+        }) 
+        // res.send(shoeModel) <- look shoe-model.js for context
+    });
+    
+app.get('/brand/:input', function(req,res) {
+    const brand = req.params.input;
+    sneaks.getProducts(brand, 6, function(err,products) {
+        products = filterShoes(products);
+        res.send(products)
+    });
+})
 
-const PORT = 3000; 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.get('/trends', function(req,res) {
+    sneaks.getMostPopular(5, function(err,products) {
+        products = filterShoes(products);
+        res.send(products);
+    })
+})
 
-
-
-
+console.log("Server now listening on http://localhost:3000/")
