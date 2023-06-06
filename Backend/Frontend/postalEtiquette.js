0d8c4c8febce4de4d0890bca4da8c7a7ed160795
LeDanse <113842990+bitparcour@users.noreply.github.com>
2023-06-06 15:28:05 +0200

    AccountMGMT SzauleLogin system done
    Backend 1file by Szaule separate  .js login, register,
    Frontend 5files by Szaule login , register postal etiquette

---
+++ Backend/Frontend/postalEtiquette.js
@@ -0,0 +1,110 @@
+// Get the form and output elements
+const form = document.getElementById('myForm');
+const output = document.getElementById('output');
+const surnameField = document.getElementById('surname');
+const nameField = document.getElementById('name');
+const addressField = document.getElementById('address');
+
+// Add event listeners to all input fields
+const fields = form.querySelectorAll('input');
+fields.forEach(field => {
+  field.addEventListener('input', updateOutput);
+});
+
+// Event listener for form submission
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get the field values
+  var surnameValue = surnameField.value;
+  var nameValue = nameField.value;
+  var addressValue = addressField.value;
+
+  // Create a new entry object with the field values
+  var entry = {
+    surname: surnameValue,
+    name: nameValue,
+    address: addressValue
+  };
+
+  // Add the new entry to the entries array
+  entries.push(entry);
+
+  // Update the textarea with all entries
+  updateOutput();
+ 
+});
+
+// Function to update the output textarea
+function updateOutput() {
+  // Get the values from the form fields
+  const surname = surnameField.value;
+  const name = nameField.value;
+  const address = addressField.value;
+
+  // Construct the output text
+  const outputText = `Surname:   ${surname}\nName:   ${name}\nAddress:   ${address}`;
+
+  // Update the output textarea with the constructed text
+  output.value = outputText;
+}
+
+
+// from the html:
+document.getElementById("submitButton").addEventListener("click", function() {
+    var surname = document.getElementById("surname").value;
+    var name = document.getElementById("name").value;
+    var address = document.getElementById("address").value;
+
+    var output = document.getElementById("output");
+    output.value = "Surname:   " + surname + "\n";
+
+    output.value += "Name:   " + name + "\n";
+    output.value += "Address:   " + address + "\n\n";
+
+
+    var customFields = document.getElementsByName("customField");
+    for (var i = 0; i < customFields.length; i++) {
+      var fieldName = customFields[i].value;
+      var fieldValueInput = document.getElementsByName(fieldName)[0];
+      output.value += fieldName + ": " + fieldValueInput.value + "\n";
+    }
+
+    document.getElementById("myForm").reset();
+    output.scrollTop = output.scrollHeight;
+  });
+
+  document.getElementById("addFieldButton").addEventListener("click", function() {
+    var form = document.getElementById("myForm");
+    var input = document.createElement("input");
+    input.type = "text";
+    input.name = "customField";
+    input.placeholder = "Field Name";
+    input.addEventListener("keyup", function(event) {
+      if (event.key === "Enter") {
+        var fieldName = event.target.value;
+        var fieldValueInput = document.createElement("input");
+        fieldValueInput.type = "text";
+        fieldValueInput.name = fieldName;
+        fieldValueInput.placeholder = fieldName +"   "+ " Value";
+        fieldValueInput.addEventListener("keyup", function() {
+          var output = document.getElementById("output");
+          output.value = ""; // Clear the textarea
+          output.value += "Surname:   " + document.getElementById("surname").value + "\n";
+          output.value += "Name:   " + document.getElementById("name").value + "\n";
+          output.value += "Address:   " + document.getElementById("address").value + "\n\n";
+
+          var customFields = document.getElementsByName("customField");
+          for (var i = 0; i < customFields.length; i++) {
+            var fieldName = customFields[i].value;
+            var fieldValueInput = document.getElementsByName(fieldName)[0];
+            output.value += fieldName + ":   " + fieldValueInput.value + "\n";
+          }
+        });
+        form.appendChild(fieldValueInput);
+        event.target.disabled = true;
+        event.target.placeholder = fieldName + " added";
+      }
+    });
+    form.appendChild(input);
+  });
--- Backend/server.js
+++ Backend/server.js
@@ -1,79 +1,31 @@
 const express = require('express');
-const path = require('path')
-const app = express()
-const SneaksAPI = require('sneaks-api')
-const sneaks = new SneaksAPI()
-const shoeModel  = require('./shoe-model.js')
+const app = express();
+const path = require('path');
+const bodyParser = require("body-parser");
+//SzauleLogin/server/backendLoginAdmin.js
+const backendLoginAdmin = require('./backendLoginAdmin');
 
-app.use(express.static(path.join(__dirname, 'Frontend')));
+// Serve static files from the "files" folder
+app.use(express.static(path.join(__dirname, 'files')));
 
-function filterShoes(shoes) {
-    var filtered = shoes.map(function(item) {      
-        return {
-            shoeName: item.shoeName,
-            brand: item.brand,
-            // colorWay: item.colorWay,
-            price: item.retailPrice,
-            thumbnail: item.thumbnail,
-            releaseDate: item.releaseDate
-        }
-    })
-    return filtered;
-}
+// Handle GET request for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'files', 'index.html'));
+});
 
-app.listen(3000)
-app.get('/all', function(req,res) {
-    let allSneaker = []
-    const getProduct1 = new Promise(function(resolve,reject) {
-        sneaks.getProducts("jordan", 5, function(err, products) {
-            products = filterShoes(products);
-            allSneaker.push(products);
-            resolve();
-        });
-     });
+// Body parser middleware
+app.use(bodyParser.json());
 
-    const getProduct2 = new Promise(function(resolve,reject) {
-        sneaks.getProducts("yeezy", 5, function(err, products) {
-            products = filterShoes(products);
-            allSneaker.push(products)
-            resolve();
-        });
-      });
-      const getProduct3 = new Promise(function(resolve,reject) {
-        sneaks.getProducts("nike", 5, function(err, products) {
-            products = filterShoes(products);
-            allSneaker.push(products)
-            resolve();
-        });
-      });
-      const getProduct4 = new Promise(function(resolve,reject) {
-        sneaks.getProducts("balance", 5, function(err, products) {
-            products = filterShoes(products);
-            allSneaker.push(products);
-            resolve();
-        })
-      })
-    
-    Promise.all([getProduct1, getProduct2, getProduct3, getProduct4])
-        .then(function() {
-          res.send(allSneaker);
-        }) 
-        // res.send(shoeModel) <- look shoe-model.js for context
-    });
-    
-app.get('/brand/:input', function(req,res) {
-    const brand = req.params.input;
-    sneaks.getProducts(brand, 6, function(err,products) {
-        products = filterShoes(products);
-        res.send(products)
-    });
-})
+// 
+//SzauleLogin/server/backendLoginAdmin.jse
+app.post('/api/login', backendLoginAdmin.handleLogin);
+app.post('/api/register', backendLoginAdmin.handleRegistration);
 
-app.get('/trends', function(req,res) {
-    sneaks.getMostPopular(6, function(err,products) {
-        products = filterShoes(products);
-        res.send(products);
-    })
-})
+const PORT = 3000; 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
-console.log("Server now listening on http://localhost:3000/")
+
+
+
