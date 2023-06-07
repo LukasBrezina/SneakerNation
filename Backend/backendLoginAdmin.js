0e9a2cbfc63be9ab4dd9eafc6746df2375ee1ad9
LeDanse <113842990+bitparcour@users.noreply.github.com>
2023-06-07 06:45:18 +0200

    seek static in Frontend

--- Backend/backendLoginAdmin.js
+++ Backend/backendLoginAdmin.js
@@ -5,12 +5,12 @@ const bodyParser = require("body-parser");
 const fs = require('fs');
 const storeUsersOnServer = 'users.json';
 
-// Serve static files from the "files" folder
-app.use(express.static(path.join(__dirname, 'files')));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'Frontend')));
 
 // Handle GET request for the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'files', 'index.html'));
+  res.sendFile(path.join(__dirname, 'Frontend', 'index.html'));
 });
 
 // Body parser middleware
