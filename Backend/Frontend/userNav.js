8ba9717960476c879c0592b0297f6ff32e17f173
LukasBrezina <lukas.brezina@stud.fh-campuswien.ac.at>
2023-06-24 16:27:33 +0200

    styling for frontLogForm and Account Button

--- Backend/Frontend/userNav.js
+++ Backend/Frontend/userNav.js
@@ -1,53 +1,57 @@
+const token = localStorage.getItem('token');
 
-    const token = localStorage.getItem('token');
-
-    fetch('/api/verify', {
-      headers: {
+fetch('/api/verify', {
+    headers: {
         Authorization: 'Bearer ' + token
-      }
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        const loginContainer = document.getElementById('loginContainer');
+
+        if (data.success) {
+            console.log(data.success)
+            // User success, load user account button
+            const userMenuHtml = `
+              <nav class="navAccount">
+            <button class="accountButton" data-toggle="collapse" data-target="#accountToggle" style="border: none; padding: 0; background: none;">
+              <img src="./images/abstract-user-flat-4.svg" width="55" height="55" alt="Profile Image"/>
+            </button>
+            <div class="collapse user-collapse" id="accountToggle">
+              <ul class="account-choices">
+                <li class="account-item">
+                  <a class="link" href="">${data.user.username}</a>
+                </li>
+                <li class="account-item">
+                  <a class="link" href="">Shopping Cart</a>
+                </li>
+                <li class="account-item">
+                  <a class="link" href="">Wishlist</a>
+                </li>
+                <li class="account-item">
+                  <a class="link" href="" id="logoutButton">Logout</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          `;
+            loginContainer.innerHTML = userMenuHtml;
+            document.getElementById('logoutButton').addEventListener('click', function(event) {
+                event.preventDefault();
+                localStorage.removeItem('token');
+                location.reload();
+            });
+        }
     })
-  .then(response => response.json())
-  .then(data => {
-    const loginContainer = document.getElementById('loginContainer');
-  
-    if (data.success) {
-      // User success, load user account button
-      const userMenuHtml = `
-        <nav class="navAccount">
-          <button class="accountButton" data-toggle="collapse" data-target="#accountToggle">Account</button>
-          <div class="collapse" id="accountToggle">
-            <ul class="account-choices">
-              <li class="account-item">
-                <a class="link" href="">${data.user.username}</a>
-              </li>
-              <li class="account-item">
-                <a class="link" href="">Shopping Cart</a>
-              </li>
-              <li class="account-item">
-                <a class="link" href="">Wishlist</a>
-              </li>
-              <li class="account-item">
-                <a class="link" href="" id="logoutButton">Logout</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      `;
-      loginContainer.innerHTML = userMenuHtml;
-       document.getElementById('logoutButton').addEventListener('click', function(event) {
-        event.preventDefault();
-        localStorage.removeItem('token');
-        location.reload();
-        });
-    } 
-  })
-  .catch(error => {
-    console.log("geht")
-    // No user, show login/register button
-    const loginRegisterHtml = `
+    .catch(error => {
+        console.log(error)
+        // No user, show login/register button
+        const loginRegisterHtml = `
       <nav class="navAccount">
-        <button class="accountButton" data-toggle="collapse" data-target="#accountToggle">Account</button>
-        <div class="collapse" id="accountToggle">
+        <button class="accountButton" data-toggle="collapse" data-target="#accountToggle" style="border: none; padding: 0; background: none;">
+          <img src="./images/abstract-user-flat-4.svg" width="55" height="55" alt="Profile Image"/>
+        </button>
+        <div class="collapse user-collapse" id="accountToggle">
           <ul class="account-choices">
             <li class="account-item">
               <a class="link" href="frontLogForm.html">Login / Register</a>
@@ -56,6 +60,6 @@
         </div>
       </nav>
     `;
-    loginContainer.innerHTML = loginRegisterHtml;
-    console.error('Error:', error);
-  });
+        loginContainer.innerHTML = loginRegisterHtml;
+        console.error('Error:', error);
+    });
